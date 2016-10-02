8806c2082401b5dfa569528bc9d761d5a107bca0
Mark Eaton <mark.eaton@kbcc.cuny.edu>
2016-10-02 16:42:21 -0400

    drying

--- find_librarians.py
+++ find_librarians.py
@@ -4,25 +4,28 @@ import pprint
 import json
 import re
 
-users = requests.get("https://api.github.com/search/users?q=librarian&per_page=100", auth=(key.keyname, key.keysecret))
-users_json = users.json()
-users_list = users_json['items']
-header_link = users.headers.get('link', None)
+url = "https://api.github.com/search/users?q=library&per_page=100"
+output = []
+
+def search(url):
+    data = requests.get(url, auth=(key.keyname, key.keysecret))
+    data_json = data.json()['items']
+    global output, header_link
+    output = output + data_json
+    header_link = data.headers.get('link', None)
+
+search(url)
 
 while header_link:
     if re.search(r'; rel="next"', header_link):
-        next_page = re.sub(r'.*<(.*)>; rel="next".*', r'\1', header_link)
-        more_data = requests.get(next_page, auth=(key.keyname, key.keysecret))
-        more_json = more_data.json()
-        more_list = more_json['items']
-        users_list = users_list + more_list 
-        break
+        url = re.sub(r'.*<(.*)>; rel="next".*', r'\1', header_link)
+        search(url)
     else:
         header_link = None
 
 #pprint.pprint(users_json)
 
-print(len(users_list))
+print(len(output))
 
 #for user in users_json['items']:
     #print(user)
