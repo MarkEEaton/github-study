f2383b51e93fbfcf2493521ba636b0311ae79936
Mark Eaton <mark.eaton@kbcc.cuny.edu>
2016-10-02 22:12:53 -0400

    create a list of librarian logins

--- find_librarians.py
+++ find_librarians.py
@@ -4,14 +4,18 @@ import pprint
 import json
 import re
 
-url = "https://api.github.com/search/users?q=library&per_page=100"
+url = "https://api.github.com/search/users?q=library&per_page=100&sort=joined&order=desc"
 output = []
+output_json = []
 
 def search(url):
     data = requests.get(url, auth=(key.keyname, key.keysecret))
     data_json = data.json()['items']
-    global output, header_link
-    output = output + data_json
+    print(data.json()['total_count'])
+    global output, output_json, header_link
+    output_json = output_json + data_json
+    for user in data_json:
+        output.append(user['login'])
     header_link = data.headers.get('link', None)
 
 search(url)
@@ -23,10 +27,5 @@ while header_link:
     else:
         header_link = None
 
-#pprint.pprint(users_json)
-
-print(len(output))
-
-#for user in users_json['items']:
-    #print(user)
-    #print(user['login'])
+print(len(output_json))
+print(output)
