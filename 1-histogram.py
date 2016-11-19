91e655450b2d37e6b146013ea6dbef9fde800345
Mark Eaton <mark.eaton@kbcc.cuny.edu>
2016-11-19 14:38:35 -0500

    finished first histogram

--- 1-histogram.py
+++ 1-histogram.py
@@ -9,25 +9,28 @@ with open('json/processedlibrarians.json', 'r') as file1:
 with open('json/processedrandoms.json', 'r') as file2:
     data2 = json.load(file2)
 
+
 def make_np(facet, group):
     facet_list = []
     i = {"librarians": data1,
          "randoms": data2}
     for user in i[group]:
-        update = datetime.datetime.strptime(i[group][user][facet], "%Y-%m-%dT%H:%M:%SZ")
-        print(update)
+        update = datetime.datetime.strptime(i[group][user][facet],
+                                            "%Y-%m-%dT%H:%M:%SZ")
         facet_list.append(update)
         mpl_list = mdate.date2num(facet_list)
     return np.array(mpl_list)
 
 x1_data = make_np("updated_at", "librarians")
-#x2_data = make_np("updated_at", "randoms")
+x2_data = make_np("updated_at", "randoms")
+
+bins = np.histogram(np.hstack((x1_data, x2_data)), bins=80)[1]
 
 fig, ax = plt.subplots()
 ax.xaxis.set_major_locator(mdate.MonthLocator())
 ax.xaxis.set_major_formatter(mdate.DateFormatter('%b'))
-plt.hist(x1_data)
-#plt.ylabel("stars")
-#plt.xlabel("followers")
+plt.hist(x1_data, bins, alpha=0.5, label='librarians')
+plt.hist(x2_data, bins, alpha=0.5, label='randoms')
+plt.legend(loc='upper left')
 
 plt.show()
