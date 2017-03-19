fd0379d6f3fc106e6069cc6b2ff6923bbc170ba3
Mark Eaton <mark.eaton@kbcc.cuny.edu>
2017-03-18 22:35:54 -0400

    improvements to 4-histogram

--- 4-histogram.py
+++ 4-histogram.py
@@ -26,16 +26,25 @@ def make_np(facet, group):
 x1_data = make_np("gh-index", "librarians")
 x2_data = make_np("gh-index", "randoms")
 
+avg_librarians = np.around(np.mean(x1_data), decimals=1)
+avg_randoms = np.around(np.mean(x2_data), decimals=1)
+
 # create the bins
-bins = np.histogram(np.hstack((x1_data, x2_data)), bins=12)[1]
+bins = np.arange(11)-0.5
 
 sns.set(style='darkgrid')
 
 # create the histogram
 fig, ax = plt.subplots()
 plt.hist([x1_data, x2_data], bins, alpha=1,
-         label=['Librarians', 'Control group' ], color=['k', '0.75'],
+         label=['Librarians', 'Control group'], color=['k', '0.75'],
          rwidth=0.75)
+plt.text(7, 66.6, 'Average gh-index for librarians: ' +
+         str(avg_librarians), ha='right', size=10)
+plt.text(7, 63.3, 'Average gh-index for control group: ' +
+         str(avg_randoms), ha='right', size=10)
+plt.xlim(-0.5, 9.5)
+plt.xticks(range(10))
 
 plt.legend(loc='upper right')
 plt.suptitle('Distribution of gh-index')
