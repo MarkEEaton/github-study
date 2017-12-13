b7b7cbe9948f4ad9dfce2fe1263a33e06eb34e6c
Mark Eaton <mark.eaton@kbcc.cuny.edu>
2017-12-13 15:52:50 -0500

    more average deltas

--- 2a-histogram.py
+++ 2a-histogram.py
@@ -4,6 +4,8 @@ import json
 import matplotlib.dates as mdate
 import datetime
 import seaborn as sns
+import arrow
+
 
 """
 Histogram of when the user was created
@@ -27,8 +29,30 @@ def make_np(facet, group):
         mpl_list = mdate.date2num(facet_list)
     return np.array(mpl_list)
 
+
 x1_data = make_np("created_at", "librarians")
 x2_data = make_np("created_at", "randoms")
+
+
+def get_avg_dates():
+    """ get the average timedeltas for both populations """
+
+    avg_librarians = np.around(np.mean(x1_data))
+    avg_randoms = np.around(np.mean(x2_data))
+
+    avg_librarians_date = mdate.num2date(avg_librarians)
+    avg_randoms_date = mdate.num2date(avg_randoms)
+
+    created_date = arrow.get(datetime.datetime(2017, 3, 1), 'US/Eastern')
+
+    librarians_delta = created_date - avg_librarians_date
+    randoms_delta = created_date - avg_randoms_date
+
+    return(librarians_delta, randoms_delta)
+
+
+avg_dates = get_avg_dates()
+avg_dates_days = (avg_dates[0].days, avg_dates[1].days)
 
 bins = np.histogram(np.hstack((x1_data, x2_data)), bins=20)[1]
 
@@ -42,6 +66,12 @@ ax.xaxis.set_major_formatter(mdate.DateFormatter('%Y'))
 # plot as a histogram
 plt.hist([x1_data, x2_data], bins, alpha=1,
          label=['Librarians', 'Control group'], color=['k', '0.75'])
+plt.text(datetime.date(2008, 6, 15), 29.85,
+         'Average days since account created for librarians: '
+         + str(avg_dates_days[0]), ha='left', size=10)
+plt.text(datetime.date(2008, 6, 15), 28.4,
+         'Average days since account created for control group: '
+         + str(avg_dates_days[1]), ha='left', size=10)
 
 plt.legend(loc='upper left')
 plt.suptitle('Date user was created')
